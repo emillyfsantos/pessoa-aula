a3f75f7a37e1a678d79418dae31bfcb934b56ecf
emillyfsantos <emilly.f.santos6@ba.estudante.senai.br>
2026-06-10 23:20:49 -0300

    Commit

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
-//criando função principal
-function App(){
+function App() {
   const [formValores, setFormValores] = useState({
-    nome:'',
-    idade: '',
-    cpf: '',
+    nome: "",
+    idade: "",
+    cpf: "",
   });
 
-  // função para capturar alterações nos inputs
   const handleChange = (e) => {
     setFormValores({
       ...formValores,
@@ -17,68 +15,70 @@ function App(){
     });
   };
 
-  // criando função para enviar formulário
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try{
+    try {
       console.log("Dados a serem enviados", formValores);
 
-      //Enviando dados via Api
-      const response = await fetch('http://localhost:3000/cadastrarPessoa',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(formValores)
+      const response = await fetch(
+        "http://localhost:3000/cadastrarPessoa",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formValores),
+        }
+      );
 
-      });
-      // convertendo a resposta para json
       const json = await response.json();
       console.log(response);
       console.log(json);
-    
-  } catch (err){
-       // Tratamento de erro
+    } catch (err) {
       console.error("Erro ao enviar", err);
-  }
+    }
+  };
 
-};
-return(
-  <div className="App">
-    <h1>Cadastro de Pessoas</h1>
-    <form onSubmit={handleSubmit}>
-      <input 
-      type="text"
-      name="nome"
-      placeholder="Nome"
-      value={formValores.nome}
-      onChange={handleChange}
-      />
-      <br/>
-      <input 
-      type="text"
-      name="cpf"
-      placeholder="CPF"
-      value={formValores.cpf}
-      onChange={handleChange}
-      />
-      <br/>
-      <input 
-      type="text"
-      name="idade"
-      placeholder="idade"
-      value={formValores.idade}
-      onChange={handleChange}
-      />
-      <br/>
-      <button type="submit">
-        Cadastrar
-      </button>
-      
+  return (
+    <div className="App">
+      <h1>Cadastro de Pessoas</h1>
 
-    </form>
-  </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nome"
+          placeholder="Nome"
+          value={formValores.nome}
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          name="cpf"
+          placeholder="CPF"
+          value={formValores.cpf}
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          name="idade"
+          placeholder="Idade"
+          value={formValores.idade}
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
   );
 }
+
 export default App;
